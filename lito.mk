06b715625e2608fcd525a7115a7129402f1047ca
Alec Chan <88945170+alecchangod@users.noreply.github.com>
2023-03-26 18:25:15 +0800

    remove calling vendor blob

--- lito.mk
+++ lito.mk
@@ -620,4 +620,4 @@ PRODUCT_BOOT_JARS += \
 
 
 # Inherit the proprietary files
-$(call inherit-product, vendor/xiaomi/sm8250-common/sm8250-common-vendor.mk)
+# $(call inherit-product, vendor/xiaomi/sm8250-common/sm8250-common-vendor.mk)
